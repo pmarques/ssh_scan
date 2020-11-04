e427a52cbdad458fe5829a7fa2c23671b99a1aed
Patrick Marques <pmarques@users.noreply.github.com>
2020-11-04 15:13:01 +0000

    temp

--- build_and_deploy_to_dockerhub.sh
+++ build_and_deploy_to_dockerhub.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-docker build -t mozilla/ssh_scan .
+docker build -t $DOCKER_USER/ssh_scan .
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     echo $DOCKER_PASS | docker login -u="$DOCKER_USER" --password-stdin;\
-    docker push mozilla/ssh_scan;\
+    docker push $DOCKER_USER/ssh_scan;\
   else
     exit 0
   fi
