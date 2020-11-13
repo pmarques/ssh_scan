b0b76b25cd524d29d0f99088bc52a2cfcb479c28
Patrick F. Marques <patrickfmarques@gmail.com>
2020-11-13 13:24:47 +0000

    temp

--- build_and_deploy_to_dockerhub.sh
+++ build_and_deploy_to_dockerhub.sh
@@ -3,12 +3,12 @@
 set -e
 
 docker buildx create --use
-docker buildx build --platform linux/arm64/v8,linux/amd64 -t mozilla/ssh_scan .
+docker buildx build --platform linux/arm64/v8,linux/amd64 -t $DOCKER_USER/ssh_scan .
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     echo $DOCKER_PASS | docker login -u="$DOCKER_USER" --password-stdin;\
-    docker push mozilla/ssh_scan;\
+    docker push $DOCKER_USER/ssh_scan;\
   else
     exit 0
   fi
